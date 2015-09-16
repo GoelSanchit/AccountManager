4a7dfbf26a44fb55801b1a290c4391b41c25e40b
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-09-16 16:22:13 +0530

    Update ListAccounts.java

--- app/src/main/java/corp/mahisan/accountmanager/ListAccounts.java
+++ app/src/main/java/corp/mahisan/accountmanager/ListAccounts.java
@@ -1,6 +1,6 @@
 package corp.mahisan.accountmanager;
 
-import android.app.Activity;
+import android.app.Activity
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
